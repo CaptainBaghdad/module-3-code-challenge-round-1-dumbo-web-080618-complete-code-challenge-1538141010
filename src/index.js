ad282516347252199295b0ca5e7defe8db434921
me <juniorjr083@gmail.com>
2018-09-28 10:38:24 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const yourUUID = 1 //Enter your assigned uuid here
+  const yourUUID = '08bb5b1f-01a7-407f-8121-d99af9a5420d';
 
   const imageURL = `https://randopic.herokuapp.com/images/${yourUUID}`
   
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 887;
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
